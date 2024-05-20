ed79b450e137b7a2ac423cb5d46d34a8ee425793
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2024-05-21 00:54:01 +0600

    Custom progressbar

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from moviepy.editor import VideoFileClip, concatenate_videoclips, vfx, Composite
 import time
 from pymediainfo import MediaInfo
 import sys
+from proglog import ProgressBarLogger
 
 class QuickMergeVideo:
     def __init__(self):
@@ -13,10 +14,14 @@ class QuickMergeVideo:
         self.__fadein = 0
         self.__fadeout = 0
     
-    def set_properties(self, fadein, fadeout, bitrate_k=200):
+    def set_properties(self, fadein, fadeout, bitrate_k=2, threads=5, subclip=False, subclip_start=None, subclip_end=None):
         self.__fadein = fadein
         self.__fadeout = fadeout
-        self.__bitrate = f"{bitrate_k}k"
+        self.__bitrate = f"{bitrate_k * 10000}k"
+        self.__threads = threads
+        self.__subclip = subclip
+        self.__subclip_start = subclip_start
+        self.__subclip_end = subclip_end
     
     def get_bitrate(self, video_path, kbps=True):
         media_info = MediaInfo.parse(video_path)
@@ -39,8 +44,12 @@ class QuickMergeVideo:
         return self.__clips
     
     def read_clips(self, message=False, clip_address=False):
+        if self.__subclip == True and not self.__subclip_start and not self.__subclip_end:
+            return
+        
         start = time.time()
-        self.__video_clips = [VideoFileClip(i).subclip(0, 5).fx(vfx.fadein, self.__fadein).fx(vfx.fadeout, self.__fadeout) for i in self.__clips]
+        self.__video_clips = [VideoFileClip(i).without_audio().fx(vfx.fadein, self.__fadein).fx(vfx.fadeout, self.__fadeout) for i in self.__clips]
+        if self.__subclip: self.__video_clips = [i.subclip(self.__subclip_start, self.__subclip_end) for i in self.__video_clips]
         end = time.time()
         
         if message: print(f"\nTotal time taken to read video files: {(end - start):.2f}s.")
@@ -49,33 +58,53 @@ class QuickMergeVideo:
     def merge_clips(self):
         self.__merged = concatenate_videoclips(self.__video_clips)
     
-    def save_video(self):
+    def save_video(self, save=True):
+        if not save: return
+        
         self.read_clips()
         self.merge_clips()
         
         start = time.time()
-        self.__merged.write_videofile("Merged.mp4", bitrate=self.__bitrate)
+        logger = CustomProgressBar()
+        self.__merged.write_videofile("Merged.mp4", bitrate=self.__bitrate, logger=logger)
         end = time.time()
         print(f"\nTotal time taken to merge videos: {(end - start):.2f}s.")
         
         log = open("log.txt", "+a")
-        log.write(f"Time: {end - start} Threads: {threads}\n")
+        log.write(f"Time: {end - start} Threads: {self.__threads}\n")
         log.close()
-        
-        
+    
+    def progress_callback(self, current, total):
+        percentage = (current / total) * 100
+        print(f"Progress: {percentage:.2f}% completed", end='\r')
+    
+    def get_duration(self):
+        self.read_clips()
+        duration = [i.duration for i in self.__video_clips]
+        print(duration)
+        print(max(duration))
 
-# clip1 = VideoFileClip("38891-418897479.mp4").subclip(0, 4).fx(vfx.fadein, 1)
-# clip2 = VideoFileClip("204307-923909646.mp4").subclip(0, 4).fx(vfx.fadein, 1)
+class CustomProgressBar(ProgressBarLogger):
+    # def callback(self, **changes):
+    #     for (parameter, value) in changes.items():
+    #         print(f"Parameter {parameter} is now {value}")
+    
+    def bars_callback(self, bar, attr, value, old_value=None):
+        percentage = (value / self.bars[bar]['total']) * 100
+        print(f"\rSaving video: {percentage:.2f}", end="\r")
+        
 
 
 if __name__ == "__main__":
     quick_video = QuickMergeVideo()
     bitrate = quick_video.get_bitrate("204307-923909646.mp4")
+    print(bitrate)
     
     if len(sys.argv) > 1:
         clips = sys.argv[1:]
         quick_video.add_clips(clips_array=clips)
-        quick_video.set_properties(fadein=1, fadeout=1)
-        quick_video.save_video()
+        quick_video.set_properties(fadein=1, fadeout=1, subclip=False, subclip_start=0, subclip_end=5)
+        quick_video.get_duration()
+        quick_video.save_video(save=False)
     
 
