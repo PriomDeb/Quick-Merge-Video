cc9f7b30183977441130a3f40bd67fc43262d16e
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2024-07-04 00:15:09 +0600

    Audio extender with args supported

--- Audio Extender/audio_extender.py
+++ Audio Extender/audio_extender.py
@@ -1,29 +1,55 @@
 from pydub import AudioSegment
-import os
+import os, sys
 import argparse
 from pydub.playback import play
 from pydub.utils import mediainfo
 import math
 
-files = [f for f in os.listdir("./") if f.endswith(".wav")]
+DEFAULT_EXTEND = 60
+DEFAULT_OVERLAP = 4
 
-audio1 = AudioSegment.from_wav(files[0])[:40000]
-extension = 5 * 60 * 1000
-overlap_duration = 4000
+parser = argparse.ArgumentParser(description="Extend a audio to given minutes.")
 
-iteration = int(extension / len(audio1))
+parser.add_argument('--audio', type=str, default=None, help='Audio path.')
+parser.add_argument('--extend', type=int, default=DEFAULT_EXTEND, help='Extend the audio to that duration mentioning minutes.')
+parser.add_argument('--overlap_duration', type=int, default=DEFAULT_OVERLAP, help='Overlapping duration in seconds.')
 
-start_time_audio_1 = len(audio1) - overlap_duration
+args = parser.parse_args()
 
-final_audio = audio1[:start_time_audio_1]
+def extend_audio(file_path=None, extend=60, overlap=4):
+    if not file_path:
+        print("Please pass a audio file. No file found.")
+        return
+    else:
+        if file_path.endswith(".mp3"):
+            print(".mp3 files are not supported.")
+            return
+    
+    audio1 = AudioSegment.from_wav(file_path)
+    extension = extend * 60 * 1000
+    overlap_duration = overlap * 1000
+    
+    iteration = int(extension / len(audio1))
+    start_time_audio_1 = len(audio1) - overlap_duration
+    
+    final_audio = audio1[:start_time_audio_1]
+    
+    for i in range(iteration):
+        overlapped = audio1[start_time_audio_1:].fade_out(overlap_duration).overlay(audio1[0:overlap_duration].fade_in(overlap_duration))
+        final_audio += overlapped + audio1[overlap_duration:]
+        
+        progress = min((i + 1) / iteration * 100, 100)
+        sys.stdout.write(f"\rProgress: {progress:.2f}%")
+        sys.stdout.flush()
+    
+    print("\nRendering audio. Please wait and don't close the program.")
+        
+    final_audio[:extension + 643].fade_out(5000).export("merged_audio.mp3", format="mp3", bitrate="320k")
+    
+    print("Rendering finished.")
 
-for i in range(iteration):
-    overlapped = audio1[start_time_audio_1:].fade_out(overlap_duration).overlay(audio1[0:overlap_duration].fade_in(overlap_duration))
-    final_audio += overlapped + audio1[overlap_duration:]
 
-final_audio[:extension + 643].fade_out(5000).export("merged_audio.mp3", format="mp3", bitrate="320k")  # 580
-
-# audio2 = AudioSegment.from_wav(files[2])  # 3600643
-# audio2 = AudioSegment.from_mp3("Sleep Music 6 (1 Hour).mp3")  # 3600692
-# print(len(audio2))
+if __name__ == "__main__":
+    audio, extend, overlap = args.audio, args.extend, args.overlap_duration
+    extend_audio(audio, extend, overlap)
 
